19f70629792ccb00a155af172abec636769815c5
Mo-Sanchez <moisessanchez6100@gmail.com>
2025-09-08 20:18:06 -0400

    Features: Added Jump Pad

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float bounce = 20f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
         
     }
 }
